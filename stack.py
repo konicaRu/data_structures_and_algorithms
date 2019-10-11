d2a8ec4e1708cee2a91b083c920ced0e0fb8f617
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-10-11 10:57:41 +0300

    Update stack.py

--- stack.py
+++ stack.py
@@ -6,15 +6,15 @@ class Stack:
         return len(self.stack)
 
     def pop(self):
-        self.stack.pop()
-        if len(self.stack) == 0:
+        if self.size() == 0:
             return None  # если стек пустой
+        self.stack.pop(0)
 
     def push(self, value):
-        self.stack.append(value)
+        self.stack.insert(0, value)
 
     def peek(self):
         if len(self.stack) == 0:
             return None  # если стек пустой
         else:
-            return self.stack[-1]
+            return self.stack[0]
