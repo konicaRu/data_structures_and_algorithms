cd6f6a83ebdfdaedd13b130d8210a6142d197282
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-10-16 21:33:42 +0300

    Update Deque_holyndron_two_side.py

--- Deque_holyndron_two_side.py
+++ Deque_holyndron_two_side.py
@@ -5,14 +5,15 @@ class Deque:
     def addFront(self, item):
         if self.size() == 0:
             self.stack.append(item)
-        else: self.stack.insert(0, item)
+        else:
+            self.stack.insert(0, item)
 
     def addTail(self, item):
         self.stack.append(item)
 
     def removeFront(self):
         if self.size() == 0:
-            return None # если очередь пуста
+            return None  # если очередь пуста
         first_el = self.stack[0]
         self.stack.pop(0)
         return first_el
@@ -31,16 +32,16 @@ class Deque:
         if self.size() == 0 or self.size() == 1:
             return 'It is polyndrom'
         while self.size() > 1:
-            if self.removeFront() == self.removeTail():
-                if self.removeFront() != self.removeTail():
-                    return 'It is not polyndrom'
-
+            if self.removeFront() != self.removeTail():
+                return 'It is not polyndrom'
 
         return 'It is polyndrom'
+
+
 deq = Deque()
-deq.addFront("t")
-deq.addFront("t1")
-deq.addTail("t1")
-deq.addFront("f2")
-deq.addTail("f2")
+deq.addFront("л")
+deq.addFront("а")
+deq.addTail("а")
+deq.addFront("8")
+deq.addTail("ш")
 print(deq.polyndrom())
