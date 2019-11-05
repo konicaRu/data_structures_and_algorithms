cd02d9e38e8a1f4431c04e0f124acd7f7d6f460b
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-11-05 21:58:30 +0300

    Update PowerSet_standart_metod_set.py

--- PowerSet_standart_metod_set.py
+++ PowerSet_standart_metod_set.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+start_time = datetime.now()
+
+
 class PowerSet:
 
     def __init__(self):
@@ -6,10 +11,7 @@ class PowerSet:
         self.slots = [None] * self.line  # создает массив с size количеством слотов
 
     def size(self):
-        count = 0
-        for i in self.slots:
-            if i != None:
-                count += 1
+        count = len(self.slots)
         return count
         # количество элементов в множестве
 
@@ -63,8 +65,36 @@ class PowerSet:
 
     def issubset(self, set2):
         inter_set = set(self.slots)
-        if set2 in inter_set:
-
+        if set2 <= inter_set:
             return True
         else:
             return False
+
+
+arr = PowerSet()
+arr.put("rrrrr")
+arr.put('vasya')
+arr.put('forest')
+arr.put('foretst')
+arr.put('forwetst')
+arr.put('forwetrst')
+arr.put('forest')
+print(arr.size())
+arr.remove('utr')
+arr.remove('forest')
+print(arr.intersection({"rrrrr", 'vasya', 'forest', 'foretst', 'forwetst'}))
+print(arr.size())
+print(arr.issubset({"rrrrr", 'vasya', 'forest', 'foretst', 'forwetst'}))
+print(arr.difference({"rrrrr", 'vasya', 'forest', 'foretst', 'fortreetst'}))
+print(arr.union({"rrrrr", 'vasya', 'forest', 'foretst', 'fortreetst'}))
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
+
+"""1. у нас разные значения могут давать одинаковые индексы по хэшу
+   2. если мы формируем множество, в котором есть только те элементы, которые имеются в каждом из множеств
+   пробегаем по базовому множеству берем значение, смотрим есть ли оно вво втором множестве, если да, берем от него хэш, смотрим по этому хэшу значение,
+    сравниваем с исходным если совпадает загоняем в новое итоговое множество, если не совпало но элемент есть в списке идем тройками дальше перебирая 
+    все ячейки
+    http://skillsmart.ru/algo/py-kf32y/s83e6f8ef537.html
+    http://skillsmart.ru/algo/py-kf32y/re8af6c877.html
+    """
