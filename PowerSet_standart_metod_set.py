d12eda77013066ebe674f37f2d0511d55afaf9a0
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-11-05 17:25:26 +0300

    add standart metod

--- PowerSet_standart_metod_set.py
+++ PowerSet_standart_metod_set.py
@@ -1,8 +1,3 @@
-from datetime import datetime
-
-start_time = datetime.now()
-
-
 class PowerSet:
 
     def __init__(self):
@@ -66,37 +61,9 @@ class PowerSet:
         return inter_set - set2
 
     def issubset(self, set2):
-        count = 0
-        for i in set2:
-            if i in self.slots and i != None:
-                count += 1
-        if count == len(set2):
+        inter_set = set(self.slots)
+        if set2 in inter_set:
+
             return True
         else:
             return False
-
-
-arr = PowerSet()
-arr.put("rrrrr")
-arr.put('vasya')
-arr.put('forest')
-arr.put('foretst')
-arr.put('forwetst')
-arr.remove('utr')
-arr.remove('forest')
-print(arr.intersection({"rrrrr", 'vasya', 'forest', 'foretst', 'fortreetst'}))
-print(arr.size())
-print(arr.issubset({"rrrrr", 'vasya', 'forest', 'foretst', 'fortreetst'}))
-print(arr.difference({"rrrrr", 'vasya', 'forest', 'foretst', 'fortreetst'}))
-print(arr.union({"rrrrr", 'vasya', 'forest', 'foretst', 'fortreetst'}))
-end_time = datetime.now()
-print('Duration: {}'.format(end_time - start_time))
-
-"""1. у нас разные значения могут давать одинаковые индексы по хэшу
-   2. если мы формируем множество, в котором есть только те элементы, которые имеются в каждом из множеств
-   пробегаем по базовому множеству берем значение, смотрим есть ли оно вво втором множестве, если да, берем от него хэш, смотрим по этому хэшу значение,
-    сравниваем с исходным если совпадает загоняем в новое итоговое множество, если не совпало но элемент есть в списке идем тройками дальше перебирая 
-    все ячейки
-    http://skillsmart.ru/algo/py-kf32y/s83e6f8ef537.html
-    http://skillsmart.ru/algo/py-kf32y/re8af6c877.html
-    """
