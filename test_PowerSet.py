a94d2f4accd0e62488c19a7c8bddd07602e5b0c6
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-11-06 15:53:09 +0300

    Add files via upload

--- test_PowerSet.py
+++ test_PowerSet.py
@@ -77,13 +77,40 @@ class MyTestCase(unittest.TestCase):
         arr.put('3')
         arr.put('4')
         arr.put('5')
-        self.assertEqual(len(arr.difference({'1','3', '7', '9', '8'})), 3)
+        self.assertEqual(len(arr.difference({'1', '3', '7', '9', '8'})), 3)
 
     def test_difference_empty(self):
         arr = PowerSet_without_hush.PowerSet()
         arr.put('1')
         arr.put('2')
         arr.put('3')
-        self.assertEqual(arr.difference({'1','2','3','12','34','56'}), None)
+        self.assertEqual(arr.difference({'1', '2', '3', '12', '34', '56'}), None)
+
+    def test_issubset(self):
+        arr = PowerSet_without_hush.PowerSet()
+        arr.put('1')
+        arr.put('2')
+        arr.put('3')
+        arr.put('4')
+        arr.put('5')
+        self.assertEqual(arr.issubset({'1', '3', '2', }), True)
+
+    def test_issubset_no_enter(self):
+        arr = PowerSet_without_hush.PowerSet()
+        arr.put('1')
+        arr.put('2')
+        arr.put('3')
+        arr.put('4')
+        arr.put('5')
+        self.assertEqual(arr.issubset({'12', '34', '56'}), False)
+
+    def test_issubset_part_enter(self):
+        arr = PowerSet_without_hush.PowerSet()
+        arr.put('1')
+        arr.put('2')
+        arr.put('3')
+        self.assertEqual(arr.issubset({'1', '2', '3', '12', '34', '56'}), False)
+
+
 if __name__ == '__main__':
     unittest.main()
