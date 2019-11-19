28e395abdd158e83f3dbafeefb4a0c420038cb7a
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-11-19 22:22:01 +0300

    add 19_11_19

--- SimpleTree.py
+++ SimpleTree.py
@@ -12,18 +12,27 @@ class SimpleTree:
         self.Root = root;  # корень, может быть None
 
     def AddChild(self, ParentNode, NewChild):
-        ParentNode.Children = NewChild
+         ParentNode.Children = NewChild #ParentNode.Children = NewChild
         # ваш код добавления нового дочернего узла существующему ParentNode
 
-
-
     def DeleteNode(self, NodeToDelete):
-        NodeToDelete.NodeValue = None
-        NodeToDelete.Parent = None
-        NodeToDelete.Children = None# ваш код удаления существующего узла NodeToDelete
+        node_up = NodeToDelete.Parent
+        if type(node_up.Children) is list:
+            node_up.Children.remove(NodeToDelete)
+            NodeToDelete.Parent = None
+            NodeToDelete.Children = None
+            NodeToDelete.NodeValue = None
+        else:
+            node_up.Children = None
+            NodeToDelete.Parent = None
+            NodeToDelete.Children = None
+            NodeToDelete.NodeValue = None
+
+        # ваш код удаления существующего узла NodeToDelete
 
     def GetAllNodes(self):
-        # ваш код выдачи всех узлов дерева в определённом порядке
+        node == self.Root
+        node1 = node# ваш код выдачи всех узлов дерева в определённом порядке
         return []
 
     def FindNodesByValue(self, val):
@@ -47,9 +56,6 @@ graf = SimpleTree(8)
 node1 = SimpleTreeNode(graf, None)
 node2 = SimpleTreeNode(2, node1)
 node3 = SimpleTreeNode(3, node2)
-node4 = SimpleTreeNode(4, node2)
 graf.AddChild(node1, node2)
 graf.AddChild(node2, node3)
-graf.AddChild(node2, [node3, node4])
-#graf.DeleteNode(node2)
-
+graf.GetAllNodes()
