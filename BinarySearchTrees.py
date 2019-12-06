7f5f54fa0cdf72eaf62533421bdd276ddf19f4c6
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-12-06 14:57:26 +0300

    add 06_12_19 14_56
    preload  add 06_12_19 14_56

---
+++ BinarySearchTrees.py
@@ -0,0 +1,141 @@
+class BSTNode:
+    def __init__(self, key, val, parent):
+        self.NodeKey = key
+        self.NodeValue = val
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+class BSTFind:
+    def __init__(self):
+        self.Node = None  # None если
+        # в дереве вообще нету узлов
+        self.NodeHasKey = False  # True если узел найден
+        self.ToLeft = False  # True, если родительскому узлу надо
+        # добавить новый узел левым потомком
+class BST:
+    def __init__(self, node):
+        self.Root = node
+    def FindNodeByKey(self, key):
+        cursor_node = self.Root
+        BSTFind.NodeHasKey = False
+        if cursor_node == None:
+            BSTFind.Node = None
+            return BSTFind
+        while True:
+            if cursor_node.NodeKey == key:
+                BSTFind.NodeHasKey = True
+                BSTFind.Node = cursor_node
+                return BSTFind
+            if cursor_node.NodeKey < key and cursor_node.RightChild != None:
+                cursor_node = cursor_node.RightChild
+                BSTFind.Node = cursor_node
+            if cursor_node.NodeKey < key and cursor_node.RightChild == None:
+                BSTFind.Node = cursor_node
+                BSTFind.ToLeft = False
+                return BSTFind
+            if cursor_node.NodeKey > key and cursor_node.LeftChild != None:
+                cursor_node = cursor_node.LeftChild
+                BSTFind.Node = cursor_node
+            if cursor_node.NodeKey > key and cursor_node.LeftChild == None:
+                BSTFind.Node = cursor_node
+                BSTFind.ToLeft = True
+                return BSTFind
+
+    def AddKeyValue(self, key, val):
+        self.FindNodeByKey(key)
+        if BSTFind.Node == None or BSTFind.NodeHasKey == True:
+          return  False
+        if BSTFind.ToLeft == False:
+           node = BSTNode(key, val, BSTFind.Node)
+           BSTFind.Node.RightChild = node
+           return
+        if BSTFind.ToLeft == True:
+           node = BSTNode(key, val, BSTFind.Node)
+           BSTFind.Node.LeftChild = node
+           return
+
+    def FinMinMax(self, FromNode, FindMax):
+        cursor_node = FromNode.Node
+        if FindMax == True:
+            while cursor_node.RightChild != None:
+                cursor_node = cursor_node.RightChild
+                if cursor_node.RightChild == None:
+                    return cursor_node
+        if FindMax == False:
+            while cursor_node.LeftChild != None:
+                cursor_node = cursor_node.LeftChild
+                if cursor_node.LeftChild == None:
+                    return cursor_node
+    def DeleteNodeByKey(self, key):
+        self.FindNodeByKey(key)
+        del_node = BSTFind.Node
+        if BSTFind.NodeHasKey == False:
+            return False
+        if del_node.LeftChild == None and del_node.RightChild == None and del_node.Parent.RightChild == del_node:
+            del_node.Parent.RightChild = None
+            del_node.Parent = None
+            BSTFind.Node = None
+            return
+        if del_node.LeftChild == None and del_node.RightChild == None and del_node.Parent.LeftChild == del_node:
+            del_node.Parent.LeftChild = None
+            del_node.Parent = None
+            BSTFind.Node = None
+            return
+        if del_node.LeftChild != None and del_node.RightChild == None:
+            del_node.LeftChild.Parent = del_node.Parent
+            if del_node.Parent.LeftChild == del_node:
+                del_node.Parent.LeftChild = del_node.LeftChild
+                BSTFind.Node = None
+                return
+            if del_node.Parent.RightChild == del_node:
+                del_node.Parent.RightChild = del_node.LeftChild
+                BSTFind.Node = None
+                return
+        if del_node.LeftChild == None and del_node.RightChild != None:
+            del_node.RightChild.Parent = del_node.Parent
+            if del_node.Parent.LeftChild == del_node:
+                del_node.Parent.LeftChild = del_node.RightChild
+                BSTFind.Node = None
+                return
+            if del_node.Parent.RightChild == del_node:
+                del_node.Parent.RightChild = del_node.RightChild
+                BSTFind.Node = None
+                return
+        if del_node.LeftChild != None and del_node.RightChild != None:
+            stop_node = del_node
+            del_node = del_node.RightChild
+            if del_node.LeftChild == None and del_node.RightChild == None:
+                del_node.Parent = stop_node.Parent
+                if stop_node.Parent.LeftChild == stop_node:
+                    stop_node.Parent.LeftChild = stop_node.RightChild
+                    stop_node.LeftChild.Parent = stop_node.RightChild
+                    stop_node.RightChild.LeftChild  = stop_node.LeftChild
+                    BSTFind.Node = None
+                if stop_node.Parent.RightChild == stop_node:
+                    stop_node.Parent.RightChild = stop_node.RightChild
+                    stop_node.LeftChild.Parent = stop_node.RightChild
+                    stop_node.RightChild.LeftChild = stop_node.LeftChild
+                    BSTFind.Node = None
+            pass
+
+    def Count(self):
+        return 0
+
+tree = BST(BSTNode(8, 18, None))
+tree.AddKeyValue(4, 4)  # формируем дерево
+tree.AddKeyValue(12, 12)
+tree.AddKeyValue(2, 2)
+tree.AddKeyValue(6, 6)
+tree.AddKeyValue(10, 10)
+tree.AddKeyValue(14, 14)
+tree.AddKeyValue(1, 1)
+tree.AddKeyValue(3, 3)
+tree.AddKeyValue(5, 5)
+tree.AddKeyValue(7, 7)
+tree.AddKeyValue(9, 9)
+tree.AddKeyValue(11, 11)
+tree.AddKeyValue(13, 13)
+tree.AddKeyValue(15, 15)
+tree.AddKeyValue(15, 15)
+tree.DeleteNodeByKey(10)
+
---
+++ Test_BinerySearchTtrees.py
@@ -0,0 +1,195 @@
+import unittest
+import BinarySearchTrees
+
+
+class MyTestCase(unittest.TestCase):
+    def test_FindNodeByKey(self):  # проверяем вставку ноды и поиск ноды
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 18, None))  # добавляем корневую ноду
+        tree.FindNodeByKey(8)  # ищем значение 8
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)  # определяем что нода найдена по параметру BSTFind.NodeHasKey = True
+        tree.AddKeyValue(4, 4)  # добавляем узел 4 к узлу 8, это левый узел
+        self.assertEqual(BinarySearchTrees.BSTFind.ToLeft, True)  # проверяем что он левый по параметру BSTFind.ToLeft = True
+        tree.AddKeyValue(4, 15)
+        self.assertEqual(tree.AddKeyValue(4, 4), False)
+        # проверить попытку добавления ключа который уже есть в дереве проверим фукцией количества узлов в дереве
+
+        tree.FindNodeByKey(6)  # ищем ноду 6 которой нет
+        self.assertEqual(BinarySearchTrees.BSTFind.Node.NodeKey == 4, True)  # определили что указатель стоит на 4
+        self.assertEqual(BinarySearchTrees.BSTFind.Node.Parent.NodeKey == 8, True)  # проверили что нода 4 имеет родителем ноду 8
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)  # определяем что нода не найдена по параметру BSTFind.NodeHasKey = Folse
+        tree.AddKeyValue(12, 12)  # добавлем ноду 6 к ноде 4 это правый узел
+        self.assertEqual(BinarySearchTrees.BSTFind.ToLeft, False)  # проверяем что он правый по параметру BSTFind.ToLeft = Folse
+        tree.FindNodeByKey(12)  # ищем ноду 6
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)  # определяем что нода найдена по параметру BSTFind.NodeHasKey = True
+        self.assertEqual(BinarySearchTrees.BSTFind.Node.Parent.RightChild == BinarySearchTrees.BSTFind.Node, True)
+        # проверили что предпоследняя 6 нода имеет правым узлом ноду 4
+        tree.FindNodeByKey(20)  # проверить что ключа нет
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)  # проверить что ключа нет
+
+    def test_FinMinMax(self):
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))  # добавляем корневую ноду
+        tree.AddKeyValue(4, 4) # формируем дерево
+        tree.AddKeyValue(12, 12)
+        tree.AddKeyValue(2, 2)
+        tree.AddKeyValue(6, 6)
+        tree.AddKeyValue(10, 10)
+        tree.AddKeyValue(14, 14)
+        tree.AddKeyValue(1, 1)
+        tree.AddKeyValue(3, 3)
+        tree.AddKeyValue(5, 5)
+        tree.AddKeyValue(7, 7)
+        tree.AddKeyValue(9, 9)
+        tree.AddKeyValue(11, 11)
+        tree.AddKeyValue(13, 13)
+        tree.AddKeyValue(15, 15)
+        # проверяем от стартовой ноды
+        start = tree.FinMinMax(tree.FindNodeByKey(8), False)
+        tree.FindNodeByKey(1)
+        self.assertEqual( start == BinarySearchTrees.BSTFind.Node, True) # находим наименьщую
+        start = tree.FinMinMax(tree.FindNodeByKey(8), True)
+        tree.FindNodeByKey(15)
+        self.assertEqual(start == BinarySearchTrees.BSTFind.Node, True) # находлим наибольшую
+        # проверяем от промежуточной ноды
+        start = tree.FinMinMax(tree.FindNodeByKey(12), False)
+        tree.FindNodeByKey(9)# находим наименьщую
+        self.assertEqual(start == BinarySearchTrees.BSTFind.Node, True)# BinarySearchTrees.BSTFind.Node ссылка на ноду при работе функции tree.FindNodeByKey(9)
+        start = tree.FinMinMax(tree.FindNodeByKey(12), True)
+        tree.FindNodeByKey(15)
+        self.assertEqual(start == BinarySearchTrees.BSTFind.Node, True)# находлим наибольшую
+
+    def test_DeleteNodeByKeyList(self):#удаляем правый лист
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))  # добавляем корневую ноду
+        tree.AddKeyValue(4, 4)  # формируем дерево
+        tree.AddKeyValue(12, 12)
+        tree.AddKeyValue(2, 2)
+        tree.AddKeyValue(6, 6)
+        tree.AddKeyValue(10, 10)
+        tree.AddKeyValue(14, 14)
+        tree.AddKeyValue(1, 1)
+        tree.AddKeyValue(3, 3)
+        tree.AddKeyValue(5, 5)
+        tree.AddKeyValue(7, 7)
+        tree.AddKeyValue(9, 9)
+        tree.AddKeyValue(11, 11)
+        tree.AddKeyValue(13, 13)
+        tree.AddKeyValue(15, 15)
+        tree.DeleteNodeByKey(1)
+        tree.FindNodeByKey(1)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.DeleteNodeByKey(3)
+        tree.FindNodeByKey(3)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.DeleteNodeByKey(11)
+        tree.FindNodeByKey(11)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.DeleteNodeByKey(9)
+        tree.FindNodeByKey(9)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+
+    def test_DeleteNodeByKeyListLeft(self):# удаляем ноду слева от родителя с левым листом
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))  # добавляем корневую ноду
+        tree.AddKeyValue(4, 4)  # формируем дерево
+        tree.AddKeyValue(12, 12)
+        tree.AddKeyValue(2, 2)
+        #tree.AddKeyValue(6, 6)
+        tree.AddKeyValue(10, 10)
+        tree.AddKeyValue(14, 14)
+        tree.DeleteNodeByKey(4)
+        tree.FindNodeByKey(4)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.FindNodeByKey(2)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)
+        tree.DeleteNodeByKey(2)
+        tree.FindNodeByKey(2)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+
+    def test_DeleteNodeByKeyListLeft2(self):  # удаляем ноду слева от родителя с правым листом
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))
+        tree.AddKeyValue(4, 4)  # формируем дерево
+        tree.AddKeyValue(12, 12)
+        #tree.AddKeyValue(2, 2)
+        tree.AddKeyValue(6, 6)
+        tree.AddKeyValue(10, 10)
+        tree.AddKeyValue(14, 14)
+        tree.DeleteNodeByKey(4)
+        tree.FindNodeByKey(4)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.FindNodeByKey(6)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)
+
+    def test_DeleteNodeByKeyListRight(self):  # удаляем ноду справа от родителя с левым листом
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))
+        tree.AddKeyValue(4, 4)  # формируем дерево
+        tree.AddKeyValue(12, 12)
+        tree.AddKeyValue(2, 2)
+        tree.AddKeyValue(6, 6)
+        tree.AddKeyValue(10, 10)
+        #tree.AddKeyValue(14, 14)
+        tree.DeleteNodeByKey(12)
+        tree.FindNodeByKey(12)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.FindNodeByKey(10)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)
+
+    def test_DeleteNodeByKeyListRight2(self):  # удаляем ноду справа от родителя с правым листом
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))
+        tree.AddKeyValue(4, 4)  # формируем дерево
+        tree.AddKeyValue(12, 12)
+        tree.AddKeyValue(2, 2)
+        tree.AddKeyValue(6, 6)
+        #tree.AddKeyValue(10, 10)
+        tree.AddKeyValue(14, 14)
+        tree.DeleteNodeByKey(12)
+        tree.FindNodeByKey(12)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.FindNodeByKey(14)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)
+
+    def test_DeleteNode2knotLeft(self): # удаляем узел с двумя дочерними листами слева от родителя
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))  # добавляем корневую ноду
+        tree.AddKeyValue(4, 4)  # формируем дерево
+        tree.AddKeyValue(12, 12)
+        tree.AddKeyValue(2, 2)
+        tree.AddKeyValue(6, 6)
+        tree.AddKeyValue(10, 10)
+        tree.AddKeyValue(14, 14)
+        tree.AddKeyValue(1, 1)
+        tree.AddKeyValue(3, 3)
+        tree.AddKeyValue(5, 5)
+        tree.AddKeyValue(7, 7)
+        tree.AddKeyValue(9, 9)
+        tree.AddKeyValue(11, 11)
+        tree.AddKeyValue(13, 13)
+        tree.AddKeyValue(15, 15)
+        tree.AddKeyValue(15, 15)
+        tree.DeleteNodeByKey(10)
+        tree.FindNodeByKey(10)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.FindNodeByKey(11)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)
+
+    def test_DeleteNode2knotRight(self): # удаляем узел с двумя дочерними листами справа от родителя
+        tree = BinarySearchTrees.BST(BinarySearchTrees.BSTNode(8, 8, None))  # добавляем корневую ноду
+        tree.AddKeyValue(4, 4)  # формируем дерево
+        tree.AddKeyValue(12, 12)
+        tree.AddKeyValue(2, 2)
+        tree.AddKeyValue(6, 6)
+        tree.AddKeyValue(10, 10)
+        tree.AddKeyValue(14, 14)
+        tree.AddKeyValue(1, 1)
+        tree.AddKeyValue(3, 3)
+        tree.AddKeyValue(5, 5)
+        tree.AddKeyValue(7, 7)
+        tree.AddKeyValue(9, 9)
+        tree.AddKeyValue(11, 11)
+        tree.AddKeyValue(13, 13)
+        tree.AddKeyValue(15, 15)
+        tree.AddKeyValue(15, 15)
+        tree.DeleteNodeByKey(14)
+        tree.FindNodeByKey(14)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, False)
+        tree.FindNodeByKey(15)
+        self.assertEqual(BinarySearchTrees.BSTFind.NodeHasKey, True)
+
+if __name__ == '__main__':
+    unittest.main()
