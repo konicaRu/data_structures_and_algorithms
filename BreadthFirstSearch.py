edf7f95657cdac21c24e130f0f9e4e9a75598278
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-12-17 20:22:20 +0300

    Update BreadthFirstSearch.py

--- BreadthFirstSearch.py
+++ BreadthFirstSearch.py
@@ -88,7 +88,7 @@ class BST:
         return tuple(vizit)
 
 
-    def DeepAllNodesRec(self, order):
+    def DeepAllNodes(self, order):
         node = self.Root
         if node == None:
             return tuple ()
@@ -122,23 +122,3 @@ class BST:
             res = res + self.PostOrder(root.RightChild)
             res.append(root)
         return res
-
-
-tree = BST(BSTNode(8, 18, None))
-tree.AddKeyValue(4, 4)  # формируем дерево
-tree.AddKeyValue(12, 12)
-# tree.AddKeyValue(2, 2)
-# tree.AddKeyValue(6, 6)
-# tree.AddKeyValue(10, 10)
-# tree.AddKeyValue(14, 14)
-# tree.AddKeyValue(1, 1)
-# tree.AddKeyValue(3, 3)
-# tree.AddKeyValue(5, 5)
-# tree.AddKeyValue(7, 7)
-# tree.AddKeyValue(9, 9)
-# tree.AddKeyValue(11, 11)
-# tree.AddKeyValue(13, 13)
-# tree.AddKeyValue(15, 15)
-print(tree.DeepAllNodesRec(1))
-print(tree.WideAllNodes())
-# https://neerc.ifmo.ru/wiki/index.php?title=%D0%94%D0%B5%D1%80%D0%B5%D0%B2%D0%BE_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA%D0%B0,_%D0%BD%D0%B0%D0%B8%D0%B2%D0%BD%D0%B0%D1%8F_%D1%80%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F
